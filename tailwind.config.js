36e78dfa97a7e4afcb49c80e4e360e15acfab742
alexChiang <alexchiang.cg@gmail.com>
2025-01-23 11:22:57 +0800

    init: first push

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // 掃描根目錄下的 HTML
+    "./src/**/*.{js,ts,jsx,tsx}", // 掃描 src 資料夾內的 React 文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
