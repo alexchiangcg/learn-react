420fe2b6e2428283b679e27956dd5be3118aab1c
alexChiang <alexchiang.cg@gmail.com>
2025-01-23 11:27:39 +0800

    chore: 測試資料

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
